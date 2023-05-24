13f521d5f60fcb6450d6e0ccd73807a69b1f49e6
JeisonRedondo <jeida16@yahoo.es>
2023-05-23 19:36:59 -0500

    Agregando funcionalidad para eliminar elementos de la lista de cart: class 26

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '@context/AppContext'
 import '@styles/OrderItem.scss';
 import closeIcon from '@icons/icon_close.png';
 
 const OrderItem = ({product}) => {
+    const { removeFromCart } = useContext(AppContext);
+
+    const handleRemove = product => {
+        removeFromCart(product);
+    }
     return (
-        <div class="OrderItem">
+        <div className="OrderItem">
             <figure>
                 <img src={product.images[0]} alt={product.title} />
             </figure>
             <p>{product.title}</p>
             <p>${ product.price}</p>
-            <img src={closeIcon} alt="close" />
+            <img src={closeIcon} alt="close"  onClick={() => handleRemove(product)}/>
         </div>
     );
 }
